21585343daeb0c110a68c9ab3955f34cd20c38c6
jombi <jombi@duck.com>
2023-11-10 16:06:27 +0900

    added icon

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,14 @@ import GithubIcon from "@/icons/GithubIcon.vue";
 
   <main>
     <div class="main">
+      <img alt="bocchi kawaii" class="profile_img" src="/bocchi.png">
       <h1>Jombi</h1>
-      <p>Evolving Backend Developer</p>
-      <div class="item-list">
-        <a href="https://instagram.com/jombidev">
+      <p class="description-text">The Evolving Backend Developer</p>
+      <div>
+        <a href="https://instagram.com/jombidev" class="icons">
           <InstagramIcon/>
         </a>
-        <a href="https://x.com/jombidev">
+        <a href="https://x.com/jombidev" class="icons">
           <TwitterIcon/>
         </a>
         <a href="https://github.com/jombidev">
@@ -28,10 +29,29 @@ import GithubIcon from "@/icons/GithubIcon.vue";
 </template>
 
 <style scoped>
+
+
+@font-face {
+  font-family: 'LINESeedKR-Bd';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2') format('woff2');
+  font-weight: 700;
+  font-style: normal;
+}
+
 main {
-  font-family: Arial, sans-serif;
+  font-family: LINESeedKR-Bd, Arial, sans-serif;
   --impact-color: #ff2291;
   --main-color: #fff000;
+}
+.profile_img {
+  width: 200px;
+  height: 200px;
+}
+.description-text {
+  padding-bottom: 10px;
+}
+.icons {
+  padding-right: 20px;
 }
 .main {
   position: absolute;
